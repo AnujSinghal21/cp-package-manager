4f19849e40670079fee73c9cd4ef731104b65e54
= <singhalanuj7575@gmail.com>
2024-08-25 12:40:21 +0530

    Started macros definitions

---
+++ lib/general/macros.cpp
@@ -0,0 +1,3 @@
+#define CPM_MACROS
+#include "../../lib/cpmlib.hpp"
+#define rep(i, s, e) for (int i = s; i < e; i++)
---
+++ lib/general/macros.hpp
@@ -0,0 +1,5 @@
+#ifndef CPM_MACROS
+#define CPM_MACROS
+#define rep(i, s, e) for (int i = s; i < e; i++)
+
+#endif
